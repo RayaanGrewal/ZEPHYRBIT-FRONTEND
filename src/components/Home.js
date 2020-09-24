b884229e8d3fcd35ab6b7b1cc358201fa2b8e408
Rayaan Grewal <rayaangrewal@gmail.com>
2020-09-24 23:36:34 +0530

    Initial commit

---
+++ src/components/Home.js
@@ -0,0 +1,20 @@
+import React , { Component } from 'react';
+
+import { Breadcrumb , Jumbotron ,  Container} from 'react-bootstrap'
+
+
+
+class Home extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+    }
+  }
+
+  render()
+  {
+  return(<div>hello</div>);
+  }
+}
+
+export default Home;
