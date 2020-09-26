972a83beae9f86cb868f2845686c055544b11cd0
Rayaan Grewal <rayaangrewal@gmail.com>
2020-09-27 01:46:38 +0530

    responsiveness fixes

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,7 +14,7 @@ class Home extends Component {
   return(
     <div>
       <NavbarComponent/>
-      <Image style={{height : 1200, width : 2000}} src={require("../assets/logo1.png")} fluid />
+      <Image src={require("../assets/logo1.png")} fluid />
     </div>
  );
   }
